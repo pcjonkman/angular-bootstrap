1fd4f9c6b239c29142b462e61e5e7fae805b2a6a
Peter Jonkman <github@pcjonkman.nl>
2022-10-21 22:41:08 +0200

    bootstrap samples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,517 @@
-import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpParams, HttpClient } from '@angular/common/http';
+import { Component, Injectable, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AbstractControl, FormControl, Validators } from '@angular/forms';
+import { NgbAlert, NgbCalendar, NgbCarouselConfig, NgbDate, NgbDateStruct, NgbModal, NgbOffcanvas, NgbPanelChangeEvent, NgbTimeStruct, NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { catchError, config, debounceTime, distinctUntilChanged, filter, map, merge, Observable, of, OperatorFunction, Subject, switchMap, tap } from 'rxjs';
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const FILTER_PAG_REGEX = /[^0-9]/g;
+
+const ALERTS: Alert[] =
+[
+  {
+    type: 'success',
+    message: 'This is an success alert',
+  }, {
+    type: 'info',
+    message: 'This is an info alert',
+  }, {
+    type: 'warning',
+    message: 'This is a warning alert',
+  }, {
+    type: 'danger',
+    message: 'This is a danger alert',
+  }, {
+    type: 'primary',
+    message: 'This is a primary alert',
+  }, {
+    type: 'secondary',
+    message: 'This is a secondary alert',
+  }, {
+    type: 'light',
+    message: 'This is a light alert',
+  }, {
+    type: 'dark',
+    message: 'This is a dark alert',
+  }
+];
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+const statesWithFlags: {name: string, flag: string}[] = [
+  {'name': 'Alabama', 'flag': '5/5c/Flag_of_Alabama.svg/45px-Flag_of_Alabama.svg.png'},
+  {'name': 'Alaska', 'flag': 'e/e6/Flag_of_Alaska.svg/43px-Flag_of_Alaska.svg.png'},
+  {'name': 'Arizona', 'flag': '9/9d/Flag_of_Arizona.svg/45px-Flag_of_Arizona.svg.png'},
+  {'name': 'Arkansas', 'flag': '9/9d/Flag_of_Arkansas.svg/45px-Flag_of_Arkansas.svg.png'},
+  {'name': 'California', 'flag': '0/01/Flag_of_California.svg/45px-Flag_of_California.svg.png'},
+  {'name': 'Colorado', 'flag': '4/46/Flag_of_Colorado.svg/45px-Flag_of_Colorado.svg.png'},
+  {'name': 'Connecticut', 'flag': '9/96/Flag_of_Connecticut.svg/39px-Flag_of_Connecticut.svg.png'},
+  {'name': 'Delaware', 'flag': 'c/c6/Flag_of_Delaware.svg/45px-Flag_of_Delaware.svg.png'},
+  {'name': 'Florida', 'flag': 'f/f7/Flag_of_Florida.svg/45px-Flag_of_Florida.svg.png'},
+  {
+    'name': 'Georgia',
+    'flag': '5/54/Flag_of_Georgia_%28U.S._state%29.svg/46px-Flag_of_Georgia_%28U.S._state%29.svg.png'
+  },
+  {'name': 'Hawaii', 'flag': 'e/ef/Flag_of_Hawaii.svg/46px-Flag_of_Hawaii.svg.png'},
+  {'name': 'Idaho', 'flag': 'a/a4/Flag_of_Idaho.svg/38px-Flag_of_Idaho.svg.png'},
+  {'name': 'Illinois', 'flag': '0/01/Flag_of_Illinois.svg/46px-Flag_of_Illinois.svg.png'},
+  {'name': 'Indiana', 'flag': 'a/ac/Flag_of_Indiana.svg/45px-Flag_of_Indiana.svg.png'},
+  {'name': 'Iowa', 'flag': 'a/aa/Flag_of_Iowa.svg/44px-Flag_of_Iowa.svg.png'},
+  {'name': 'Kansas', 'flag': 'd/da/Flag_of_Kansas.svg/46px-Flag_of_Kansas.svg.png'},
+  {'name': 'Kentucky', 'flag': '8/8d/Flag_of_Kentucky.svg/46px-Flag_of_Kentucky.svg.png'},
+  {'name': 'Louisiana', 'flag': 'e/e0/Flag_of_Louisiana.svg/46px-Flag_of_Louisiana.svg.png'},
+  {'name': 'Maine', 'flag': '3/35/Flag_of_Maine.svg/45px-Flag_of_Maine.svg.png'},
+  {'name': 'Maryland', 'flag': 'a/a0/Flag_of_Maryland.svg/45px-Flag_of_Maryland.svg.png'},
+  {'name': 'Massachusetts', 'flag': 'f/f2/Flag_of_Massachusetts.svg/46px-Flag_of_Massachusetts.svg.png'},
+  {'name': 'Michigan', 'flag': 'b/b5/Flag_of_Michigan.svg/45px-Flag_of_Michigan.svg.png'},
+  {'name': 'Minnesota', 'flag': 'b/b9/Flag_of_Minnesota.svg/46px-Flag_of_Minnesota.svg.png'},
+  {'name': 'Mississippi', 'flag': '4/42/Flag_of_Mississippi.svg/45px-Flag_of_Mississippi.svg.png'},
+  {'name': 'Missouri', 'flag': '5/5a/Flag_of_Missouri.svg/46px-Flag_of_Missouri.svg.png'},
+  {'name': 'Montana', 'flag': 'c/cb/Flag_of_Montana.svg/45px-Flag_of_Montana.svg.png'},
+  {'name': 'Nebraska', 'flag': '4/4d/Flag_of_Nebraska.svg/46px-Flag_of_Nebraska.svg.png'},
+  {'name': 'Nevada', 'flag': 'f/f1/Flag_of_Nevada.svg/45px-Flag_of_Nevada.svg.png'},
+  {'name': 'New Hampshire', 'flag': '2/28/Flag_of_New_Hampshire.svg/45px-Flag_of_New_Hampshire.svg.png'},
+  {'name': 'New Jersey', 'flag': '9/92/Flag_of_New_Jersey.svg/45px-Flag_of_New_Jersey.svg.png'},
+  {'name': 'New Mexico', 'flag': 'c/c3/Flag_of_New_Mexico.svg/45px-Flag_of_New_Mexico.svg.png'},
+  {'name': 'New York', 'flag': '1/1a/Flag_of_New_York.svg/46px-Flag_of_New_York.svg.png'},
+  {'name': 'North Carolina', 'flag': 'b/bb/Flag_of_North_Carolina.svg/45px-Flag_of_North_Carolina.svg.png'},
+  {'name': 'North Dakota', 'flag': 'e/ee/Flag_of_North_Dakota.svg/38px-Flag_of_North_Dakota.svg.png'},
+  {'name': 'Ohio', 'flag': '4/4c/Flag_of_Ohio.svg/46px-Flag_of_Ohio.svg.png'},
+  {'name': 'Oklahoma', 'flag': '6/6e/Flag_of_Oklahoma.svg/45px-Flag_of_Oklahoma.svg.png'},
+  {'name': 'Oregon', 'flag': 'b/b9/Flag_of_Oregon.svg/46px-Flag_of_Oregon.svg.png'},
+  {'name': 'Pennsylvania', 'flag': 'f/f7/Flag_of_Pennsylvania.svg/45px-Flag_of_Pennsylvania.svg.png'},
+  {'name': 'Rhode Island', 'flag': 'f/f3/Flag_of_Rhode_Island.svg/32px-Flag_of_Rhode_Island.svg.png'},
+  {'name': 'South Carolina', 'flag': '6/69/Flag_of_South_Carolina.svg/45px-Flag_of_South_Carolina.svg.png'},
+  {'name': 'South Dakota', 'flag': '1/1a/Flag_of_South_Dakota.svg/46px-Flag_of_South_Dakota.svg.png'},
+  {'name': 'Tennessee', 'flag': '9/9e/Flag_of_Tennessee.svg/46px-Flag_of_Tennessee.svg.png'},
+  {'name': 'Texas', 'flag': 'f/f7/Flag_of_Texas.svg/45px-Flag_of_Texas.svg.png'},
+  {'name': 'Utah', 'flag': 'f/f6/Flag_of_Utah.svg/45px-Flag_of_Utah.svg.png'},
+  {'name': 'Vermont', 'flag': '4/49/Flag_of_Vermont.svg/46px-Flag_of_Vermont.svg.png'},
+  {'name': 'Virginia', 'flag': '4/47/Flag_of_Virginia.svg/44px-Flag_of_Virginia.svg.png'},
+  {'name': 'Washington', 'flag': '5/54/Flag_of_Washington.svg/46px-Flag_of_Washington.svg.png'},
+  {'name': 'West Virginia', 'flag': '2/22/Flag_of_West_Virginia.svg/46px-Flag_of_West_Virginia.svg.png'},
+  {'name': 'Wisconsin', 'flag': '2/22/Flag_of_Wisconsin.svg/45px-Flag_of_Wisconsin.svg.png'},
+  {'name': 'Wyoming', 'flag': 'b/bc/Flag_of_Wyoming.svg/43px-Flag_of_Wyoming.svg.png'}
+];
+
+type State = {id: number, name: string};
+
+const statesTypeahead6: State[] = [
+  {id: 0, name: 'Alabama'},
+  {id: 1, name: 'Alaska'},
+  {id: 2, name: 'American Samoa'},
+  {id: 3, name: 'Arizona'},
+  {id: 4, name: 'Arkansas'},
+  {id: 5, name: 'California'},
+  {id: 6, name: 'Colorado'},
+  {id: 7, name: 'Connecticut'},
+  {id: 8, name: 'Delaware'},
+  {id: 9, name: 'District Of Columbia'},
+  {id: 10, name: 'Federated States Of Micronesia'},
+  {id: 11, name: 'Florida'},
+  {id: 12, name: 'Georgia'},
+  {id: 13, name: 'Guam'},
+  {id: 14, name: 'Hawaii'},
+  {id: 15, name: 'Idaho'},
+  {id: 16, name: 'Illinois'},
+  {id: 17, name: 'Indiana'},
+  {id: 18, name: 'Iowa'},
+  {id: 19, name: 'Kansas'},
+  {id: 20, name: 'Kentucky'},
+  {id: 21, name: 'Louisiana'},
+  {id: 22, name: 'Maine'},
+  {id: 23, name: 'Marshall Islands'},
+  {id: 24, name: 'Maryland'},
+  {id: 25, name: 'Massachusetts'},
+  {id: 26, name: 'Michigan'},
+  {id: 27, name: 'Minnesota'},
+  {id: 28, name: 'Mississippi'},
+  {id: 29, name: 'Missouri'},
+  {id: 30, name: 'Montana'},
+  {id: 31, name: 'Nebraska'},
+  {id: 32, name: 'Nevada'},
+  {id: 33, name: 'New Hampshire'},
+  {id: 34, name: 'New Jersey'},
+  {id: 35, name: 'New Mexico'},
+  {id: 36, name: 'New York'},
+  {id: 37, name: 'North Carolina'},
+  {id: 38, name: 'North Dakota'},
+  {id: 39, name: 'Northern Mariana Islands'},
+  {id: 40, name: 'Ohio'},
+  {id: 41, name: 'Oklahoma'},
+  {id: 42, name: 'Oregon'},
+  {id: 43, name: 'Palau'},
+  {id: 44, name: 'Pennsylvania'},
+  {id: 45, name: 'Puerto Rico'},
+  {id: 46, name: 'Rhode Island'},
+  {id: 47, name: 'South Carolina'},
+  {id: 48, name: 'South Dakota'},
+  {id: 49, name: 'Tennessee'},
+  {id: 50, name: 'Texas'},
+  {id: 51, name: 'Utah'},
+  {id: 52, name: 'Vermont'},
+  {id: 53, name: 'Virgin Islands'},
+  {id: 54, name: 'Virginia'},
+  {id: 55, name: 'Washington'},
+  {id: 56, name: 'West Virginia'},
+  {id: 57, name: 'Wisconsin'},
+  {id: 58, name: 'Wyoming'}
+];
+
+const WIKI_URL = 'https://en.wikipedia.org/w/api.php';
+const PARAMS = new HttpParams({
+  fromObject: {
+    action: 'opensearch',
+    format: 'json',
+    origin: '*'
+  }
+});
+
+@Injectable()
+export class WikipediaService {
+  constructor(private http: HttpClient) {}
+
+  search(term: string) {
+    if (term === '') {
+      return of([]);
+    }
+
+    return this.http
+      .get<[any, string[]]>(WIKI_URL, {params: PARAMS.set('search', term)}).pipe(
+        map(response => response[1])
+      );
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WikipediaService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  _success = new Subject<string>();
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+  alerts: Alert[] = [];
+  successMessage = '';
+  disabled = false;
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  isCollapsed = false;
+  model: NgbDateStruct = { day: 1, month: 1, year: 2000 };
+  date: {year: number, month: number} = { year: 2000, month: 1 };
+  displayMonths = 2;
+  navigation = 'select';
+  showWeekNumbers = false;
+  outsideDays = 'visible';
+  hoveredDate: NgbDate | null = null;
 
-  constructor(private modalService: NgbModal) {
+  fromDate: NgbDate | null = null;
+  toDate: NgbDate | null = null;
+
+  closeResult: string = '';
+  page = 4;
+  pageAdvanced = 7;
+  name = 'World';
+  currentRate = 8;
+  currentRateDec = 3.14;
+  selected = 0;
+  hovered = 0;
+  readonly = false;
+  disabledPanel = false;
+
+  time = {hour: 13, minute: 30};
+  meridian = true;
+  timeTp: NgbTimeStruct = {hour: 13, minute: 30, second: 30};
+  seconds = true;
+  timeCs: NgbTimeStruct = {hour: 13, minute: 30, second: 0};
+  hourStep = 1;
+  minuteStep = 15;
+  secondStep = 30;
+  show = true;
+  showToast = false
+  autohide = true;
+  typeaheadmodel: any;
+  typeaheadmodel2: any;
+  typeaheadmodel3: any;
+  typeaheadmodel4: any;
+  typeaheadmodel5: any;
+  typeaheadmodel6: any;
+  searching = false;
+  searchFailed = false;
+  formatter = (result: string) => result.toUpperCase();
+  formatterTypeahead5 = (x: {name: string}) => x.name;
+  formatterTypeahead6 = (state: State) => state.name;
+  ctrl = new FormControl<number | null>(null, Validators.required);
+  ctrlCs = new FormControl<NgbTimeStruct | null>(null, (control: AbstractControl ) => {
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+
+    if (value.hour < 12) {
+      return {tooEarly: true};
+    }
+    if (value.hour > 13) {
+      return {tooLate: true};
+    }
+
+    return null;
+  });
+
+  @ViewChild('selfClosingAlert', {static: false}) selfClosingAlert: NgbAlert | undefined;
+  @ViewChild('instance', { static: true })
+  instance!: NgbTypeahead;
+
+  constructor(private modalService: NgbModal, private config: NgbCarouselConfig, private calendar: NgbCalendar, private offcanvasService: NgbOffcanvas, private _service: WikipediaService) {
+    this.reset();
+    // customize default values of carousels used by this component tree
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
   }
 
-  public open(modal: any): void {
+  ngOnInit(): void {
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.selfClosingAlert) {
+        this.selfClosingAlert.close();
+      }
+    });
+  }
+
+  beforeChange($event: NgbPanelChangeEvent) {
+
+    if ($event.panelId === 'preventchange-2') {
+      $event.preventDefault();
+    }
+
+    if ($event.panelId === 'preventchange-3' && $event.nextState === false) {
+      $event.preventDefault();
+    }
+  }
+
+  searchTypeahead: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  );
+
+  searchTypeahead2: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? states
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+    );
+  };
+
+  searchTypeahead3: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term === '' ? []
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  );
+
+  searchTypeahead4: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      tap(() => this.searching = true),
+      switchMap(term =>
+        this._service.search(term).pipe(
+          tap(() => this.searchFailed = false),
+          catchError(() => {
+            this.searchFailed = true;
+            return of([]);
+          }))
+      ),
+      tap(() => this.searching = false)
+    );
+
+  searchTypeahead5: OperatorFunction<string, readonly {name: any, flag: any}[]> = (text$: Observable<string>) => text$.pipe(
+    debounceTime(200),
+    map(term => term === '' ? []
+      : statesWithFlags.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  );
+
+  searchTypeahead6: OperatorFunction<string, readonly {id: any, name: any}[]> = (text$: Observable<string>) => text$.pipe(
+    debounceTime(200),
+    distinctUntilChanged(),
+    filter(term => term.length >= 2),
+    map(term => statesTypeahead6.filter(state => new RegExp(term, 'mi').test(state.name)).slice(0, 10))
+  );
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+  }
+
+  selectToday() {
+    this.model = this.calendar.getToday();
+  }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  }
+
+  changeSuccessMessage() { this._success.next(`${new Date()} - Message successfully changed.`); }
+
+  open(modal: any): void {
     this.modalService.open(modal);
   }
 
+  openBackDropCustomClass(content: any) {
+    this.modalService.open(content, {backdropClass: 'light-blue-backdrop'});
+  }
+
+  openWindowCustomClass(content: any) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+
+  openSm(content: any) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openXl(content: any) {
+    this.modalService.open(content, { size: 'xl' });
+  }
+
+  openFullscreen(content: any) {
+    this.modalService.open(content, { fullscreen: true });
+  }
+
+  openVerticallyCentered(content: any) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  openScrollableContent(longContent: any) {
+    this.modalService.open(longContent, { scrollable: true });
+  }
+
+  openModalDialogCustomClass(content: any) {
+    this.modalService.open(content, { modalDialogClass: 'dark-modal' });
+  }
+
+  openEnd(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'end' });
+  }
+
+  openTop(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'top' });
+  }
+
+  openBottom(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'bottom' });
+  }
+
+  openNoBackdrop(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { backdrop: false });
+  }
+
+  openScroll(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { scroll: true });
+  }
+
+  openNoKeyboard(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { keyboard: false });
+  }
+
+  openNoAnimation(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { animation: false });
+  }
+
+  openCustomBackdropClass(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { backdropClass: 'bg-info' });
+  }
+
+  openCustomPanelClass(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { panelClass: 'bg-info' });
+  }
+
+  getPageSymbol(current: number) {
+    return ['A', 'B', 'C', 'D', 'E', 'F', 'G'][current - 1];
+  }
+
+  selectPage(page: string) {
+    this.page = parseInt(page, 10) || 1;
+  }
+
+  formatInput(input: HTMLInputElement) {
+    input.value = input.value.replace(FILTER_PAG_REGEX, '');
+  }
+
+  toggle() {
+    if (this.ctrl.disabled) {
+      this.ctrl.enable();
+    } else {
+      this.ctrl.disable();
+    }
+  }
+
+  toggleMeridian() {
+      this.meridian = !this.meridian;
+  }
+
+  toggleSeconds() {
+    this.seconds = !this.seconds;
+  }
+
+  closeToast() {
+    this.show = false;
+    setTimeout(() => this.show = true, 3000);
+  }
+
+  toggleWithGreeting(tooltip: any, greeting: string) {
+    if (tooltip.isOpen()) {
+      tooltip.close();
+    } else {
+      tooltip.open({greeting});
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbdSortableHeader } from './table/sortable.directive';
+import { NgbdTableComplete } from './table/table-complete';
+import { NgbdTimepickerAdapter } from './timepicker-adapter/timepicker-adapter';
+import { ToastsContainer } from './toast/toast-container.component';
+import { NgbdToastGlobal } from './toast/toast-global.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdTableComplete,
+    NgbdSortableHeader,
+    NgbdTimepickerAdapter,
+    NgbdToastGlobal,
+    ToastsContainer
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     NgbModule,
     AppRoutingModule
   ],
